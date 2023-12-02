87b4c48c0b95b74055e146fd64e09f7114dd6d34
osushinekotan <spmt3710@gmail.com>
2023-12-02 11:39:54 +0000

    add : utils

---
+++ src/experiment/utils.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.model_selection import BaseCrossValidator
+
+
+def assign_fold_index(
+    train_df: pd.DataFrame,
+    kfold: BaseCrossValidator,
+    y_col: str,
+    group_col: str | None = None,
+) -> pd.DataFrame:
+    train_df["fold"] = -1
+
+    strategy = (
+        kfold.split(X=train_df, y=train_df[y_col])
+        if group_col is None
+        else kfold.split(X=train_df, y=train_df[y_col], groups=train_df[group_col])
+    )
+    for fold_index, (_, valid_index) in enumerate(strategy):
+        train_df.loc[valid_index, "fold"] = fold_index
+    return train_df
